70d8257c5d05e5dd375fa490652dfbc96c14cdac
Steven Buschbach <sbuschbach@uchicago.edu>
2020-06-03 23:03:25 -0500

    steve wed evening update

--- scripts/fit_models_with_pca.py
+++ scripts/fit_models_with_pca.py
@@ -3,16 +3,16 @@ import pipeline
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression, Lasso, Ridge
-from sklearn.svm import SVR, LinearSVR
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.ensemble import AdaBoostRegressor
+from sklearn.svm import LinearSVR
+from joblib import dump
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 
-def build_and_split_master_df():
+def build_and_split_master_df(save_path):
     """
     Function to call other scripts that build the final dataset.
     This also does some processing, such as doing a train-test
@@ -27,26 +27,39 @@ def build_and_split_master_df():
     main_target  = 'retail_and_recreation_percent_change_from_baseline'
     features     = [col for col in raw.columns if (col not in index_vars) and (col not in target_vars)]
 
-    # Get full dataset for use
+    # Get full dataset for use and drop unnecessary variables
     df = raw.dropna(subset=[main_target])[features+[main_target]+['date']]
     df.drop(columns=['state_x','state_y','precip_dummy_x','precip_dummy_y','county',
                     'stay_at_home_announced','stay_at_home_effective'], inplace=True)
 
     # Split train test 
-    train_full,test_full = pipeline.get_train_test(df,train_size=0.8,time_series=True)
+    train_full,validation_full,test_full = pipeline.get_train_test(df,train_size=0.8,
+                                                                time_series=True,validation=True)
     train_target = train_full[main_target]
+    validation_target = validation_full[main_target]
     test_target = test_full[main_target]
     train_features = train_full.drop(columns=[main_target])
+    validation_features = validation_full.drop(columns=[main_target])
     test_features = test_full.drop(columns=[main_target])
 
     # Impute and normalize
-    train_features,test_features = pipeline.impute_missing(train_features,test_features,how='median')
-    train_features,test_features = pipeline.normalize_vars(train_features,test_features)
+    train_features,validation_features,test_features = pipeline.impute_missing(train_features,test_features,
+                                                                               validation_features,how='median')
+    train_features,validation_features,test_features = pipeline.normalize_vars(train_features,test_features,
+                                                                               validation_features)
 
-    return train_features,test_features,train_target,test_target
+    # Save output
+    dump(train_features, save_path+"/Data - Train Features.joblib")
+    dump(validation_features, save_path+"/Data - Validation Features.joblib")
+    dump(test_features, save_path+"/Data - Test Features.joblib")
+    dump(train_target, save_path+"/Data - Train Target.joblib")
+    dump(validation_target, save_path+"/Data - Validation Target.joblib")
+    dump(test_target, save_path+"/Data - Test Target.joblib")                              
+
+    return train_features,validation_features,test_features,train_target,validation_target,test_target
 
 
-def get_pca_reduction(train_features,test_features):
+def get_pca_reduction(train_features,validation_features,test_features,save_path):
     """
     Gets a dimensionality-reduced version of datasets
     using PCA. Chooses number of components so as to 
@@ -63,13 +76,19 @@ def get_pca_reduction(train_features,test_features):
     pca = PCA(n_components=num_components)
     pca.fit(train_features)
     train_pca_features = pca.transform(train_features)
+    validation_pca_features = pca.transform(validation_features)
     test_pca_features  = pca.transform(test_features)
 
-    return train_pca_features,test_pca_features
+    # Save output
+    dump(train_pca_features, save_path+"/Data - Train Features PCA.joblib")
+    dump(validation_pca_features, save_path+"/Data - Validation Features PCA.joblib")
+    dump(test_pca_features, save_path+"/Data - Test Features PCA.joblib")
+
+    return train_pca_features,validation_pca_features,test_pca_features
 
 
 def fit_and_eval_models(train_pca_features,train_target,
-                        test_pca_features,test_target,
+                        validation_pca_features,validation_target,
                         save_path):
     """
     Function to fit a number of regression models,
@@ -97,23 +116,31 @@ def fit_and_eval_models(train_pca_features,train_target,
                                 'n_jobs':-1} \
                                 for y in ['auto','log2','sqrt']
                                 for x in [100,500,1000]],
-        'AdaBoostRegressor': [{'n_estimators':y} for y in [50,75,100,125,150,200]],
+        'AdaBoostRegressor': [{'n_estimators':y} for y in [50,75,100,125,150,175,200]],
         'KNeighborsRegressor': [{'n_neighbors':x} for x in np.arange(5,20)]
     }
 
     # Fit and get results
     model_results = pipeline.build_regressors(MODELS, GRID,
                                               train_pca_features, train_target,
-                                              test_pca_features, test_target,
+                                              validation_pca_features, validation_target,
                                               save_path)
     return model_results
 
 
 if __name__ == "__main__":
 
-    train_features,test_features,train_target,test_target = build_and_split_master_df()
-    train_pca_features,test_pca_features = get_pca_reduction(train_features,test_features)
+    save_path = "../output"
+    train_features,validation_features,test_features,train_target,validation_target,test_target = build_and_split_master_df(save_path)
+    train_pca_features,validation_pca_features,test_pca_features = get_pca_reduction(train_features,
+                                                                                     validation_features,
+                                                                                     test_features,
+                                                                                     save_path)
     model_results = fit_and_eval_models(train_pca_features,train_target,
-                                        test_pca_features,test_target,
-                                        "../output")
-    model_results.to_csv("../data/model_results.csv")
+                                        validation_pca_features,validation_target,
+                                        save_path)
+    
+    # Sort and save results
+    model_results = model_results.sort_values('MAE')
+    model_results.to_csv("../output/model_validation_results_with_pca.csv")
+
---
+++ scripts/fit_models_without_pca.py
@@ -0,0 +1,105 @@
+import build_master_df
+import pipeline
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import LinearSVR
+from joblib import dump
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+def build_and_split_master_df(save_path):
+    """
+    Function to call other scripts that build the final dataset.
+    This also does some processing, such as doing a train-test
+    split. (where test data = 20% most recent dates observed)
+    """
+
+    raw = build_master_df.build_df()
+
+    # Identify variables
+    index_vars   = ['StateName','CountyName','fips','date']
+    target_vars  = [col for col in raw.columns if (col.endswith('change_from_baseline'))]
+    main_target  = 'retail_and_recreation_percent_change_from_baseline'
+    features     = [col for col in raw.columns if (col not in index_vars) and (col not in target_vars)]
+
+    # Get full dataset for use and drop unnecessary variables
+    df = raw.dropna(subset=[main_target])[features+[main_target]+['date']]
+    df.drop(columns=['state_x','state_y','precip_dummy_x','precip_dummy_y','county',
+                    'stay_at_home_announced','stay_at_home_effective'], inplace=True)
+
+    # Split train test 
+    train_full,validation_full,test_full = pipeline.get_train_test(df,train_size=0.8,
+                                                                time_series=True,validation=True)
+    train_target = train_full[main_target]
+    validation_target = validation_full[main_target]
+    test_target = test_full[main_target]
+    train_features = train_full.drop(columns=[main_target])
+    validation_features = validation_full.drop(columns=[main_target])
+    test_features = test_full.drop(columns=[main_target])
+
+    # Impute and normalize
+    train_features,validation_features,test_features = pipeline.impute_missing(train_features,test_features,
+                                                                               validation_features,how='median')
+    train_features,validation_features,test_features = pipeline.normalize_vars(train_features,test_features,
+                                                                               validation_features)
+
+    # Save output
+    dump(train_features, save_path+"/Data - Train Features.joblib")
+    dump(validation_features, save_path+"/Data - Validation Features.joblib")
+    dump(test_features, save_path+"/Data - Test Features.joblib")
+    dump(train_target, save_path+"/Data - Train Target.joblib")
+    dump(validation_target, save_path+"/Data - Validation Target.joblib")
+    dump(test_target, save_path+"/Data - Test Target.joblib")                              
+
+    return train_features,validation_features,test_features,train_target,validation_target,test_target
+
+def fit_and_eval_models(train_features,train_target,
+                        validation_features,validation_target,
+                        save_path):
+    """
+    Function to fit a number of regression models,
+    evaluate, and return the results. This takes a while to run.
+    """
+
+    # Config: Dictionaries of models and hyperparameters
+    MODELS = {
+        'LinearRegression': LinearRegression(), 
+        'Lasso': Lasso(),
+        'RandomForestRegressor': RandomForestRegressor(),
+        'AdaBoostRegressor':AdaBoostRegressor(),
+        'KNeighborsRegressor':KNeighborsRegressor()
+    }
+    GRID = {
+        'LinearRegression': [{}],
+        'Lasso': [{'alpha':x, 'random_state':0, 'max_iter':10000} for x in [0.01,0.05,0.1,0.5,1,5,10,50,100,500,1000]],
+        'RandomForestRegressor': [{'n_estimators':x, 'max_features':'log2',
+                                   'n_jobs':-1} \
+                                    for x in [100,500,1000]],
+        'AdaBoostRegressor': [{'n_estimators':y} for y in [50,75,100,125,150,175,200]],
+    }
+
+    # Fit and get results
+    model_results = pipeline.build_regressors(MODELS, GRID,
+                                              train_features, train_target,
+                                              validation_features, validation_target,
+                                              save_path)
+    return model_results
+
+
+if __name__ == "__main__":
+
+    save_path = "../output"
+    train_features,validation_features,test_features,train_target,validation_target,test_target = build_and_split_master_df(save_path)
+    model_results = fit_and_eval_models(train_features,train_target,
+                                        validation_features,validation_target,
+                                        save_path)
+    
+    # Sort and save results
+    model_results = model_results.sort_values('MAE')
+    model_results.to_csv("../output/model_validation_results_without_pca.csv")
+
